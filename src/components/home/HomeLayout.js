af6d29196543c38b81520a0f52ecd1f4ad17fbc8
Yiran <562751689@qq.com>
2019-10-15 08:12:46 +0800

    update

--- src/components/home/HomeLayout.js
+++ src/components/home/HomeLayout.js
@@ -1,43 +1,97 @@
 import React from 'react'
 import Navbar from "../common/navbar";
 import CustomeCard from "./customeCard";
-import {Grid, Image, Visibility, Segment} from 'semantic-ui-react'
+import {Grid, Image, Visibility, Segment,Container} from 'semantic-ui-react'
+import Carousel from 'rmc-m-carousel';
 
+const HomeLayout = () => {
+    const items = [{
+        image: require("../../assets/640.jpg"),
+        title: '图片1',
+    }, {
+        image: require("../../assets/641.jpg"),
+        title: '图片2',
+    }, {
+        image: require("../../assets/642.jpg"),
+        title: '图片3',
+    }, {
+        image: require("../../assets/643.jpg"),
+        title: '图片4',
+    }];
 
-const HomeLayout = () => (
-    <Visibility
+    return <Visibility
         once={false}
     >
+
         <Segment
 
-            textAlign='center'
+
             style={{padding: '0em 0em'}}
             vertical
         >
 
-            <Image fluid  src={require("../../assets/0.jpg")} alt="" style={{position:"absolute"}} />
-            <Navbar />
+
+            <Navbar fixed={true}/>
         </Segment>
-        <Grid columns='8' style={{ marginTop: "25%"}} >
+
+
+        {/**/}
+
+
+
+    <Grid style={{marginTop: ""}}>
+        <Grid.Row textAlign='center'>
+
+            <Grid.Column width={4}>
+            </Grid.Column>
+            <Grid.Column width={8}>
+                <Container>
+                    <Carousel style={{height:"30%"}}>
+                        <a target="_blank" rel="nofollow me noopener noreferrer" >
+                            <img src={require("../../assets/640.jpg")} alt="" style={{width:"100%",height:"300px"}}/>
+                        </a>
+                        <a target="_blank" rel="nofollow me noopener noreferrer" >
+                            <img src={require("../../assets/641.jpg")} alt="" style={{width:"100%",height:"300px"}}/>
+                        </a>
+                        <a target="_blank" rel="nofollow me noopener noreferrer" >
+                            <img src={require("../../assets/642.jpg")} alt="" style={{width:"100%",height:"300px"}}/>
+                        </a>
+                        <a target="_blank" rel="nofollow me noopener noreferrer" >
+                            <img src={require("../../assets/643.jpg")} alt="" style={{width:"100%",height:"300px"}}/>
+                        </a>
+                    </Carousel>
+                </Container>
+            </Grid.Column>
+            <Grid.Column width={4}>
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
+
+        <Grid columns='8' style={{marginTop: "5%"}}>
+
+
             <Grid.Row className={"segment"} textAlign='center'>
                 <Grid.Column/>
                 <Grid.Column/>
                 <Grid.Column
                     children={
-                        <CustomeCard img={<Image src={require("../../assets/p1.png")} wrapped/>}
+                        <CustomeCard img={<Image src={require("../../assets/01.jpg")} style={{height: "200px"}} fuild/>}
                                      content="中介语语料" active={0}/>}
                 />
                 <Grid.Column
-                    children={<CustomeCard img={<Image src={require("../../assets/p2.png")} wrapped/>}
-                                           content="汉语教材语料" active={1}/>}
+                    children={<CustomeCard
+                        img={<Image src={require("../../assets/02.jpg")} style={{height: "200px"}} fuild/>}
+                        content="汉语教材语料" active={1}/>}
                 />
                 <Grid.Column
-                    children={<CustomeCard img={<Image src={require("../../assets/p3.png")} wrapped/>}
-                                           content="语法项目信息检索" active={2}/>}
+                    children={<CustomeCard
+                        img={<Image src={require("../../assets/03.jpg")} style={{height: "200px"}} fuild/>}
+                        content="语法项目信息检索" active={2}/>}
                 />
                 <Grid.Column
-                    children={<CustomeCard img={<Image src={require("../../assets/p4.png")} wrapped/>}
-                                           content="文献推荐" active={3}/>}
+                    children={<CustomeCard
+                        img={<Image src={require("../../assets/04.jpg")} style={{height: "200px"}} fuild/>}
+                        content="文献推荐" active={3}/>}
                 />
                 <Grid.Column/>
                 <Grid.Column/>
@@ -45,6 +99,6 @@ const HomeLayout = () => (
         </Grid>
     </Visibility>
 
-)
+}
 
 export default HomeLayout
